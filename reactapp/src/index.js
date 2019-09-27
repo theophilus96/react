f34e9d28994dabbfc12438b65fbd2c5c738aab87
theo <theophilus96@gmail.com>
2019-09-27 22:35:11 +0800

    Initial commit

---
+++ reactapp/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import { tsConstructSignatureDeclaration } from '@babel/types';
+
+const element = <h1>Hello World</h1>;
+ReactDOM.render(element, document.getElementById('root'));
